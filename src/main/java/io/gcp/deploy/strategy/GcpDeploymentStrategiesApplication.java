2dab019f2867608f4e95da62523c50edb5e572ac
GagandeepBudhiraja <gagandeep.singh2045@gmail.com>
2020-01-12 23:58:26 +0530

    @Scheduled @EnableScheduling configured

--- src/main/java/io/gcp/deploy/strategy/GcpDeploymentStrategiesApplication.java
+++ src/main/java/io/gcp/deploy/strategy/GcpDeploymentStrategiesApplication.java
@@ -4,14 +4,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class GcpDeploymentStrategiesApplication {
 	Logger log = LoggerFactory.getLogger(this.getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(GcpDeploymentStrategiesApplication.class, args);
+	}
+
+	@Scheduled(fixedDelay = 3000)
+	public void printScheduled() {
+		log.info("Time is " + System.currentTimeMillis());
 	}
 
 }
